27d930e3c60f5dd71bc4898e1b9a1ace048d496f
Khaoula Najmeddine <khaoula.najmeddine@bordeaux-inp.fr>
2026-03-31 22:40:58 +0200

    Commiting more than one file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,42 @@
 // eslint.config.mjs
-import withNuxt from './.nuxt/eslint.config.mjs'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
 import vueParser from 'vue-eslint-parser'
 import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
-export default withNuxt({
-  files: ['**/*.vue'],
-  languageOptions: {
-    parser: vueParser,
-    parserOptions: {
-      parser: tsParser,
-      sourceType: 'module',
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-undef': 'off',
+      'vue/no-unused-vars': 'error',
+      'no-unused-vars': 'error',
     },
   },
-  rules: {
-    'no-undef': 'off', 
-    'vue/no-unused-vars': 'error',
-    'no-unused-vars': 'error'
-  }
-})
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'error',
+    },
+  },
+  {
+    ignores: ['.nuxt/**', '.output/**', 'node_modules/**'],
+  },
+]
